f266f5ef3bea61ccc4fd6ec094c95870d6497633
Victor Leite <victor_leite@outlook.com.br>
2025-02-08 12:05:53 -0300

    fix context test

--- src/main/java/com/fiap/fiap_video_extractor/api/handlers/ExtractionProcessorListener.java
+++ src/main/java/com/fiap/fiap_video_extractor/api/handlers/ExtractionProcessorListener.java
@@ -6,9 +6,11 @@ import com.fiap.fiap_video_extractor.core.exceptions.ExtractionAlreadyBeingProce
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class ExtractionProcessorListener {
 
     private static final Logger log = LoggerFactory.getLogger(ExtractionProcessorListener.class);
--- src/test/java/com/fiap/fiap_video_extractor/FiapVideoExtractorApplicationTests.java
+++ src/test/java/com/fiap/fiap_video_extractor/FiapVideoExtractorApplicationTests.java
@@ -2,12 +2,13 @@ package com.fiap.fiap_video_extractor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FiapVideoExtractorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
